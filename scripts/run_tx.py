ce9842180c37c3785d3c9d30d4e28df70d49967b
Andrew Tu <drewtu2@yahoo.com>
2020-03-14 00:18:56 -0400

    Lifi Tx runningn on pi

--- scripts/run_tx.py
+++ scripts/run_tx.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python
 
 #import unicornhat as unicorn
-import UnicornWrapper as unicorn 
+import sys
+import os
+path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+if not path in sys.path:
+    sys.path.insert(1, path)
+del path
+
+import Lifi.UnicornWrapper as unicorn 
 
 import sys
 import os
@@ -9,6 +16,7 @@ path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 if not path in sys.path:
     sys.path.insert(1, path)
 del path
+import weakref
 
 from Lifi.tx import LifiTx
 
@@ -17,6 +25,6 @@ if __name__ == "__main__":
     unicorn.rotation(0)
     unicorn.brightness(0.5)
 
-    mymsg = 64
-    tx = LifiTx(unicorn)
+    mymsg = 69
+    tx = LifiTx(weakref.ref(unicorn))
     tx.run(mymsg)
